95a843067ba7c7adbe982fb4b27beafbd17abedd
Tharusha Amarasinghe <ta2617@ic.ac.uk>
2020-12-29 00:39:02 +0000

    Added c stream implementation

--- video_stream/basic_stream.sh
+++ video_stream/basic_stream.sh
@@ -1,20 +1,5 @@
-gst-launch-1.0 rpicamsrc preview=false rotation=180 name=videosrc bitrate=2000000 \
+gst-launch-1.0 rpicamsrc preview=false rotation=180 annotation-mode=time+date name=videosrc bitrate=2000000 \
 ! h264parse \
 ! video/x-h264,framerate=25/1,width=1280,height=720 \
 ! matroskamux \
-! tcpclientsink host=192.168.0.79 port=5000
-# ! tcpserversink host=192.168.0.101 port=5000 \
-
-
-# # -v = verbose
-# gst-launch-1.0 -c rpicamsrc preview=false bitrate=1000000 \
-# ! h264parse \
-# ! rtph264pay config-interval=1 pt=96 \
-# ! gdppay \
-# ! gdpdepay \
-# ! rtph264depay \
-# ! avdec_h264 \
-# ! autovideosink sync=false
-# # ! tcpserversink host=192.168.0.101 port=5000 \
-
-
+! tcpserversink host=192.168.0.77 port=5000

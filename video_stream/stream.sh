693f12d3d4a899148c89d903c069cc43eaee63f6
Tharusha Amarasinghe <ta2617@ic.ac.uk>
2021-01-28 16:33:35 +0000

    Added udp simple stream

---
+++ video_stream/stream.sh
@@ -0,0 +1,7 @@
+gst-launch-1.0 rpicamsrc rotation=180 preview=false bitrate=1000000 \
+    ! 'video/x-h264,width=640,height=480' \
+    ! h264parse \
+    ! queue \
+    ! rtph264pay config-interval=1 pt=96 \
+    ! gdppay \
+    ! udpsink host=192.168.0.79 port=5000

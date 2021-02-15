5d5dbefbefc15a607bae18d0e1a1565ac94955e0
Tharusha Amarasinghe <ta2617@ic.ac.uk>
2021-02-15 14:06:31 +0000

    Cyclic bitrate

--- python_gstreamer/stream.py
+++ python_gstreamer/stream.py
@@ -3,13 +3,17 @@
 
 import sys
 import gi
+import random
 gi.require_version('Gst', '1.0')
-from gi.repository import GObject, Gst
+from gi.repository import GObject, Gst, GLib
 
 HOSTIP = "192.168.0.99"
-BITRATE = "2000000"
+# BITRATE = "2000000"
 STREAM_PARAMS = "framerate=25/1,width=1280,height=720"
 
+bitrate = 2000000
+rates = [2000, 20000, 200000, 2000000]
+random.seed()
 def bus_call(bus, msg, *args):
     # print("BUSCALL", msg, msg.type, *args)
     if msg.type == Gst.MessageType.EOS:
@@ -22,33 +26,34 @@ def bus_call(bus, msg, *args):
         return
     return True
 
-saturation = -100
-def set_saturation(pipeline):
-    global saturation
-    if saturation <= 100:
-      print("Setting saturation to {0}".format(saturation))
-      videosrc.set_property("saturation", saturation)
-      videosrc.set_property("annotation-text", "Saturation %d" % (saturation))
-    else:
-      pipeline.send_event (Gst.Event.new_eos())
-      return False
-    saturation += 10
+def set_bitrate(pipeline):
+    global bitrate
+    print("Setting bitrate to {0}".format(bitrate))
+    videosrc.set_property("bitrate", bitrate)
+    videosrc.set_property("annotation-text", "Bitrate %d" % (bitrate))
+    # if saturation <= 100:
+    # else:
+    #   pipeline.send_event (Gst.Event.new_eos())
+    #   return False
+    # bitrate += 10
+    bitrate = random.choice(rates)
     return True
 
 
 if __name__ == "__main__":
-    GObject.threads_init()
+    # GObject.threads_init()
     # initialization
-    loop = GObject.MainLoop()
+    loop = GLib.MainLoop()
     Gst.init(None)
 
     # pipeline = Gst.parse_launch ("rpicamsrc name=src ! video/x-h264,width=320,height=240 ! h264parse ! mp4mux ! filesink name=s")
     pipeline = Gst.parse_launch(f"\
-    rpicamsrc preview=false rotation=180 annotation-mode=time+date name=src bitrate={BITRATE} \
+    rpicamsrc preview=false rotation=180 annotation-mode=time+date name=src \
     ! video/x-h264,{STREAM_PARAMS} \
     ! h264parse \
     ! queue \
     ! rtph264pay config-interval=1 pt=96 \
+    ! gdppay \
     ! udpsink host={HOSTIP} port=5000")
 
     if pipeline == None:
@@ -61,11 +66,12 @@ if __name__ == "__main__":
     bus.add_watch(0, bus_call, loop)
 
     videosrc = pipeline.get_by_name ("src")
-    videosrc.set_property("saturation", saturation)
+    videosrc.set_property("bitrate", bitrate)
+    # videosrc.set_property("saturation", saturation)
     videosrc.set_property("annotation-mode", 1)
 
-    # this will call set_saturation every 1s
-    GObject.timeout_add(1000, set_saturation, pipeline)
+    # this will call set_saturation every 5s
+    GLib.timeout_add(5000, set_bitrate, pipeline)
 
     # run
     pipeline.set_state(Gst.State.PLAYING)

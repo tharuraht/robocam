c64e94fdacbd348ec9596d93c129c381e86d5eaf
Tharusha Amarasinghe <ta2617@ic.ac.uk>
2021-04-17 17:13:47 +0100

    reexperimenting with udp

---
+++ host_machine/receiver/udp_receive.py
@@ -0,0 +1,121 @@
+#!/usr/bin/python3
+import sys
+import gi
+gi.require_version('Gst', '1.0')
+from gi.repository import Gst, GLib
+import rec_bitrate
+import json
+from multiprocessing import Process
+import socket
+
+#hostname = '10.200.200.1'
+hostname = '0.0.0.0'
+
+class video_receiver:
+    # initialization
+    loop = GLib.MainLoop()
+    Gst.init(None)
+    rec_bitrate = 0
+    stream_started = False
+
+    def __init__(self, conf):
+        self.conf = conf
+
+    def bus_call(self, bus, msg, *args):
+        # print("BUSCALL", msg, msg.type, *args)
+        if msg.type == Gst.MessageType.EOS:
+            print("End-of-stream")
+            self.loop.quit()
+            return
+        elif msg.type == Gst.MessageType.ERROR:
+            print("GST ERROR", msg.parse_error())
+            self.loop.quit()
+            return
+        elif msg.type == Gst.MessageType.STREAM_START:
+            print("Stream Started!")
+            self.stream_started = True
+        return True
+
+    def update_bitrate(self, pipeline):
+        self.rec_bitrate = rec_bitrate.get_bitrate(time=2)
+        tcp_ip = self.conf['pi']['vpn_addr']
+        tcp_port = self.conf['pi']['comms_port']
+        if self.stream_started:
+            print(f"connecting to {tcp_ip} {tcp_port}")
+            rec_bitrate.send_bitrate(tcp_ip, tcp_port, self.rec_bitrate)
+        # print('Update finished')
+        return True
+
+    def launch(self):
+        hostport = self.conf['host']['stream_rec_port']
+        pipeline = Gst.parse_launch(f"\
+        udpsrc port={hostport} name=src \
+        ! gdpdepay \
+        ! rtph264depay \
+        ! avdec_h264 \
+        ! videoconvert \
+        ! autovideosink sync=false")
+
+        if pipeline == None:
+            print ("Failed to create pipeline")
+            sys.exit(0)
+
+        # watch for messages on the pipeline's bus (note that this will only
+        # work like this when a GLib main loop is running)
+        bus = pipeline.get_bus()
+        bus.add_watch(0, self.bus_call, self.loop)
+
+        # GLib.timeout_add(5000, self.update_bitrate, pipeline)
+
+        # run
+        pipeline.set_state(Gst.State.PLAYING)
+        try:
+            self.loop.run()
+        except Exception as e:
+            print(e)
+        # cleanup
+        pipeline.set_state(Gst.State.NULL)
+
+def calculate_bitrate(conf):
+    tcp_port = conf['host']['comms_port']
+
+    print(f'Hosting server on port {tcp_port}')
+
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    s.bind(('', tcp_port))
+    s.listen()
+    print("Server is open to connections")
+    try:
+        while True:
+            try:
+                conn, addr = s.accept()
+                # print("Connection address:", addr)
+                if addr[0] == conf['pi']['vpn_addr']:
+                    # Measure bitrate and send to pi
+                    rate = rec_bitrate.get_bitrate(time=2)
+                    msg = f"REC_BITRATE:{rate}"
+                    print('Sending', msg)
+                    conn.sendall(bytearray(msg,'utf-8'))
+                else:
+                    print(f'{addr} is not a valid source address')
+            finally:
+                conn.close()
+    finally:
+        s.close()
+
+
+
+if __name__ == "__main__":
+    with open("robocam_conf.json") as conf_file:
+        conf = json.load(conf_file)
+    
+    rec = video_receiver(conf)
+    bitrate_proc = Process(target=calculate_bitrate, args=(conf,))
+    bitrate_proc.start()
+
+    receiver_proc = Process(target=rec.launch())
+    receiver_proc.start()
+
+    receiver_proc.join()
+    bitrate_proc.terminate()
---
+++ robot/stream/udp_stream.py
@@ -0,0 +1,137 @@
+#!/usr/bin/python3
+# http://lifestyletransfer.com/how-to-launch-gstreamer-pipeline-in-python/
+
+import sys
+import gi
+gi.require_version('Gst', '1.0')
+from gi.repository import GObject, Gst, GLib
+import json
+import socket
+import os
+
+
+class video_streamer:
+     # initialization
+    loop = GLib.MainLoop()
+    Gst.init(None)
+    # bitrate = 0
+    rate_scaling_factor = 1.2
+
+    def __init__(self, conf):
+        self.conf = conf
+        self.bitrate = 200000
+    
+    def bus_call(self, bus, msg, *args):
+        # print("BUSCALL", msg, msg.type, *args)
+        if msg.type == Gst.MessageType.EOS:
+            print("End-of-stream")
+            self.loop.quit()
+            return
+        elif msg.type == Gst.MessageType.ERROR:
+            print("GST ERROR", msg.parse_error())
+            self.loop.quit()
+            return
+        elif msg.type == Gst.MessageType.STREAM_START:
+            print("Stream Started!")
+        return True
+    
+    def set_bitrate(self, videosrc):
+        print("Updating video bitrate to {0}".format(self.bitrate))
+        videosrc.set_property("bitrate", self.bitrate)
+        # videosrc.set_property("annotation-text", "Bitrate %d" % (self.bitrate))
+
+        new_rate = self.get_rec_bitrate()
+        
+        if new_rate and self.bitrate != new_rate:
+            self.bitrate = new_rate
+            print('Configured next bitrate set to', self.bitrate)
+        return True
+
+    def get_rec_bitrate(self):
+        tcp_ip = self.conf['host']['vpn_addr']
+        tcp_port = self.conf['host']['comms_port']
+        buff_sz = 100
+
+        rate = 0
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        try:
+            s.connect((tcp_ip, tcp_port))
+            # print('connected')
+            while True:
+                data = s.recv(buff_sz)
+                if data:
+                    msg = data.decode('utf-8')
+                    # print('msg',msg)
+                    ctrl, val = msg.split(':')
+                    if ctrl == 'REC_BITRATE':
+                        rate =  self.scaled_bitrate(float(val))
+                        # print('Setting NET rate to', rate)
+                else:
+                    break
+                    
+        except ConnectionRefusedError:
+            print('Unable to connect')
+        except Exception as e:
+            print(e)
+        finally:
+            s.close()
+            print("Receiver rate", rate)
+            return rate
+
+    
+    def scaled_bitrate(self, rate):
+        return min(int(self.rate_scaling_factor*rate), GLib.MAXINT)
+
+    
+    def launch(self):
+        hostip = self.conf['host']['stream_hostname']
+        stream_params = self.conf['pi']['stream_params']
+        hostport = self.conf['host']['stream_rec_port']
+
+        pipeline = Gst.parse_launch(f"\
+        rpicamsrc preview=true rotation=180 annotation-mode=time+date name=src \
+        ! video/x-h264,{stream_params} \
+        ! h264parse \
+        ! queue \
+        ! rtph264pay config-interval=1 pt=96 \
+        ! gdppay \
+        ! queue \
+        ! udpsink host={hostip} port={hostport}")
+
+        if pipeline == None:
+            print ("Failed to create pipeline")
+            sys.exit(0)
+
+        # watch for messages on the pipeline's bus (note that this will only
+        # work like this when a GLib main loop is running)
+        bus = pipeline.get_bus()
+        bus.add_watch(0, self.bus_call, self.loop)
+
+        videosrc = pipeline.get_by_name ("src")
+        videosrc.set_property("bitrate", self.bitrate)
+
+        GLib.timeout_add(5000, self.set_bitrate, videosrc)
+
+        # run
+        pipeline.set_state(Gst.State.PLAYING)
+        try:
+            self.loop.run()
+        except Exception as e:
+            print(e)
+        finally:
+            # cleanup
+            pipeline.set_state(Gst.State.NULL)
+
+
+def write_bitrate(rate):
+    print(rate)
+    with open('net_bitrate.tmp','w') as f:
+        f.write(rate)
+
+
+if __name__ == "__main__":
+    with open("robocam_conf.json") as conf_file:
+        conf = json.load(conf_file)
+
+    streamer = video_streamer(conf)
+    streamer.launch()
